625266ba78bb0896d06aab27a260b12ce3d2037a
xujingchengqaz <53473732+xujingchengqaz@users.noreply.github.com>
2019-08-02 10:14:18 +0800

    sp-02itemservice

---
+++ sp02-itemservice/src/main/java/com/tedu/sp02/Sp02ItemServiceApplication.java
@@ -0,0 +1,15 @@
+package com.tedu.sp02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Sp02ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp02ItemServiceApplication.class, args);
+	}
+
+}
